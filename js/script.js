e5712cd114dbb675448105fcdee3e35d07e60b8f
imaginamaria <emaria@imagina.com.uy>
2024-11-11 13:44:37 -0300

    resolvi las cards, agregue, section chinfield, nav transparente scroll

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Navbar transparente al scrollear
+
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('navbar-sticky');
+      } else {
+        navbar.classList.remove('navbar-sticky');
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', userScroll);
+  
