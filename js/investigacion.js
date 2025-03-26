1993a5defcc7ba43260a45b1ab912c7567bee015
imaginamaria <emaria@imagina.com.uy>
2025-03-26 12:18:43 -0300

    corregi el acordion en investigacion

--- js/investigacion.js
+++ js/investigacion.js
@@ -55,5 +55,71 @@ const mostrarError = (error) => {
     imprimir("articulosContainer-error", error);
 }
 
+// Acordion temas
+function actualizarAcordeon(articulos) {
+    if (!Array.isArray(articulos) || articulos.length === 0) {
+        console.warn("No hay artículos válidos para actualizar el acordeón.");
+        return;
+    }
+
+    articulos.forEach(articulo => {
+        if (!Array.isArray(articulo.temas) || articulo.temas.length === 0) {
+            console.warn(`El artículo "${articulo.titulo}" no tiene temas válidos.`);
+            return;
+        }
+
+        articulo.temas.forEach((tema) => {
+            let ulId = "";
+            switch (tema.trim()) {
+                case "Sanidad Animal":
+                    ulId = "salud";
+                    break;
+                case "Biotecnología":
+                    ulId = "biotecnologia";
+                    break;
+                case "Investigación Veterinaria":
+                    ulId = "investigacion";
+                    break;
+                case "Desarrollo de Productos":
+                    ulId = "desarrollo";
+                    break;
+                default:
+                    console.warn(`No se encontró una lista para el tema: ${tema}`);
+                    return;
+            }
+
+            const ul = document.getElementById(ulId);
+            if (ul) {
+                const li = document.createElement("li");
+                li.innerHTML = `<a href="articulo.html?id=${articulo.id}">${articulo.titulo}</a>`;
+                ul.appendChild(li);
+            }
+        });
+    });
+};
+
+// Función para inicializar la página
+async function inicializarPagina() {
+    try {
+        const articulos = await RequestsAPI.getArticulos();
+        actualizarAcordeon(articulos);
+
+        // Obtenemos el artículo por su id y luego los articulos relacionados
+        RequestsAPI.getArticulo(idArticulo)
+            .then(data => {
+                if (!data) {
+                    throw new Error("Articulo no encontrado");
+                }
+                mostrarDetalle(data);
+            })
+            .catch((error) => {
+                mostrarError(error);
+            });
+    } catch (error) {
+        console.error("Error al inicializar la página:", error);
+        mostrarError("Error al cargar la página. Por favor, intenta nuevamente."); // Mostrar mensaje de error en la interfaz
+    }
+}
+
 // obtenemos las cards de articulos
 RequestsAPI.getArticulos().then(mostrarCardArticulos).catch(mostrarError);
